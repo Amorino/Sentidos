a1b662795ffb05ae19085645dcfbb0bb49e3a870
Erick Bazan <erick@foostudio.mx>
2015-06-19 19:12:25 -0500

    -

--- include/Resources.h
+++ include/Resources.h
@@ -3,6 +3,8 @@
 
 //#define RES_MY_RES			CINDER_RESOURCE( ../resources/, image_name.png, 128, IMAGE )
 
-
+#define RES_KESTREL			CINDER_RESOURCE( ../resources/, output.msh, 128, MESH )
+#define RES_KESTREL_OBJ			CINDER_RESOURCE( ../resources/, untitled.obj, 128, DATA )
+#define RES_SENTIDO_OBJ			CINDER_RESOURCE( ../resources/, sentido.obj, 128, MESH )
 
 
---
+++ xcode/build/Sentidos.build/Debug/Sentidos.build/Objects-normal/x86_64/SentidosApp.d
@@ -0,0 +1,7 @@
+dependencies: /Users/foostudio/Desktop/code/Sentidos/src/SentidosApp.cpp \
+  ../../Cinder/cinder_master/include/cinder/ObjLoader.h \
+  ../../Cinder/cinder_master/include/cinder/app/RendererGl.h \
+  ../../Cinder/cinder_master/include/cinder/CameraUi.h \
+  ../../Cinder/cinder_master/include/cinder/Rand.h \
+  /Users/foostudio/Desktop/code/Sentidos/include/Resources.h \
+  ../../Cinder/cinder_master/include/cinder/CinderResources.h
